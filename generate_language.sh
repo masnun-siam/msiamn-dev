c7c4182b48bd0320fe3198d1d5aa79f798ff63c1
Masnun Siam <masnunsiam@gmail.com>
2024-03-15 23:12:47 +0600

    2024-03-15_23:12:47

--- generate_language.sh
+++ generate_language.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/Users/siam/development/flutter/bin/cache/dart-sdk/bin/dart run easy_localization:generate -S assets/translations -f keys -O lib/src/localization/generated -o locale_keys.g.dart
-/Users/siam/development/flutter/bin/cache/dart-sdk/bin/dart run easy_localization:generate -S assets/translations -f json -O lib/src/localization/generated -o locale_json.g.dart
+dart run easy_localization:generate -S assets/translations -f keys -O lib/src/localization/generated -o locale_keys.g.dart
+dart run easy_localization:generate -S assets/translations -f json -O lib/src/localization/generated -o locale_json.g.dart
